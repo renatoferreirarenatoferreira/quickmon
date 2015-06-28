630ac0841c2e788aa66d1cbfce8be7adc10d1e1e
Renato A. Ferreira <renatoferreirarenatoferreira@hotmail.com>
2015-06-28 19:05:40 -0300

    First version of Ping Tool is now released. It shows statistics about the remote host as well as real time graphic with latency data.

--- pingwindow.cpp
+++ pingwindow.cpp
@@ -8,6 +8,7 @@ PingWindow::PingWindow(QWidget* parent) :
     ui->setupUi(this);
     //window flags
     this->setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowMinimizeButtonHint);
+    this->setAttribute(Qt::WA_DeleteOnClose, true);
     //drop behavior
     setAcceptDrops(true);
 
@@ -16,11 +17,31 @@ PingWindow::PingWindow(QWidget* parent) :
     //create the timer object
     this->asyncWorker = new QTimer(this);
     connect(this->asyncWorker, SIGNAL(timeout()), this, SLOT(asyncTask()));
+
+    //format plot
+    ui->plotLatency->addGraph();
+    ui->plotLatency->graph(0)->setPen(QPen(Qt::darkGreen));
+    ui->plotLatency->graph(0)->setBrush(QBrush(QColor(230, 230, 230)));
+    ui->plotLatency->graph(0)->setAntialiasedFill(false);
+    //
+    ui->plotLatency->xAxis->setTickLabelType(QCPAxis::ltDateTime);
+    ui->plotLatency->xAxis->setDateTimeFormat("hh:mm:ss");
+    ui->plotLatency->xAxis->setAutoTickStep(false);
+    ui->plotLatency->xAxis->setTickStep(25);
+
+    //reset values
+    this->resetValues();
 }
 
 PingWindow::~PingWindow()
 {
+    //stop tasks
+    this->asyncWorker->stop();
     delete this->asyncWorker;
+
+    //disable callback
+    if (this->pigingContext != NULL)
+        this->pigingContext->listener = NULL;
 
     delete ui;
 }
@@ -43,6 +64,8 @@ void PingWindow::dropEvent(QDropEvent* event)
 void PingWindow::run(int hostID)
 {
     this->asyncWorker->stop();
+    //reset values
+    this->resetValues();
 
     QSqlQuery query = LocalData::Instance()->createQuery("SELECT * FROM hosts WHERE ID=:ID");
     query.bindValue(":ID", hostID);
@@ -50,8 +73,23 @@ void PingWindow::run(int hostID)
 
     if (query.next())
     {
-        ui->label_Name->setText(query.value("name").toString());
-        ui->label_Address->setText(query.value("address").toString());
+        ui->labelName->setText(query.value("name").toString());
+        ui->labelAddress->setText(query.value("address").toString());
+
+        //start immediately if adress is valid IPv4/6
+        this->pingingAddress.setAddress(query.value("address").toString());
+        if (this->pingingAddress.protocol() == QAbstractSocket::IPv4Protocol ||
+            this->pingingAddress.protocol() == QAbstractSocket::IPv6Protocol)
+        {
+            latencyCalculator.reset();
+            this->waitingForReply = true;
+            this->pigingContext = Pinger::Instance()->ping(this->pingingAddress, this);
+            this->asyncWorker->start(1000);
+            this->setWindowTitle("Pinging " + query.value("address").toString());
+        } else {
+            this->setWindowTitle("Ping: Looking up for hostname " + query.value("address").toString());
+        }
+        //always look up for host name
         QHostInfo::lookupHost(query.value("address").toString(), this, SLOT(lookupHostReply(QHostInfo)));
     }
 }
@@ -60,23 +98,158 @@ void PingWindow::lookupHostReply(QHostInfo hostInfo)
 {
     if (!hostInfo.addresses().isEmpty())
     {
+        //update address label
         this->pingingAddress = hostInfo.addresses().first();
+        ui->labelAddress->setText(this->pingingAddress.toString()+" ("+hostInfo.hostName()+")");
+        this->setWindowTitle("Pinging " + this->pingingAddress.toString()+" ("+hostInfo.hostName()+")");
 
-        ui->label_Address->setText(this->pingingAddress.toString()+" ("+hostInfo.hostName()+")");
-        Pinger::Instance()->ping(this->pingingAddress, this);
-        this->asyncWorker->start(1000);
+        //start pinging if not started already
+        if (!this->asyncWorker->isActive())
+        {
+            latencyCalculator.reset();
+            this->waitingForReply = true;
+            this->pigingContext = Pinger::Instance()->ping(this->pingingAddress, this);
+            this->asyncWorker->start(1000);
+        }
     } else {
         QMessageBox::warning(this, "Hostname lookup", "Address cannot be resolved or is invalid!");
+        this->setWindowTitle("Ping: Address cannot be resolved or is invalid!");
     }
 }
 
 void PingWindow::receivePingReply(PingContext* context)
 {
+    //ignore delayed responses
+    if (context->requestAddress != this->pingingAddress)
+        return;
+
+    //update ui
+    QMetaObject::invokeMethod(this, "updateUI", Qt::QueuedConnection,
+                              Q_ARG(int, context->replyStatus),
+                              Q_ARG(double, context->millisLatencyHighResolution));
+
+    //reset context reference
+    this->pigingContext = NULL;
+
+    //retry immetdiatelly if this reply took too long to arrive
+    if (this->retryImmediatelly)
+    {
+        this->pigingContext = Pinger::Instance()->ping(this->pingingAddress, this);
+        this->retryImmediatelly = false;
+    } else {
+        this->waitingForReply = false;
+    }
+
+#ifdef QT_DEBUG
     if (context->replyStatus == PINGER_STATUS_SUCCESS)
         qDebug() << "Reply from " << context->replyAddress.toString() << " time(HR/LR)=" << context->millisLatencyHighResolution << "/" << context->millisLatencyLowResolution;
+    else
+        qDebug() << "Timeout!";
+    qDebug() << "     LastLatency=" << latencyCalculator.lastLatency() << " AvgLatency=" << latencyCalculator.averageLatency() << " AvgJitter=" << latencyCalculator.averageJitter() << " PctJitter=" << latencyCalculator.percentJitter() << " PctLoss=" << latencyCalculator.percentLoss() << "%";
+#endif
 }
 
 void PingWindow::asyncTask()
 {
-    Pinger::Instance()->ping(this->pingingAddress, this);
+    if (!this->waitingForReply && !this->retryImmediatelly)
+    {
+        this->waitingForReply = true;
+        this->pigingContext = Pinger::Instance()->ping(this->pingingAddress, this);
+    } else {
+        this->retryImmediatelly = true;
+    }
+}
+
+void PingWindow::resetValues()
+{
+    this->pigingContext = NULL;
+    ui->labelLastRTT->setText("0.0 ms");
+    ui->labelAvgRTT->setText("0.0 ms");
+    ui->labelAvgJitter->setText("0.0 ms");
+    ui->labelPctLoss->setText("0.0 %");
+    ui->labelPctLoss->setStyleSheet(STYLE_FIELD_NORMAL);
+    ui->labelPctLossLabel->setStyleSheet(STYLE_LABEL_NORMAL);
+    ui->plotLatency->graph(0)->clearData();
+}
+
+void PingWindow::updateUI(int replyStatus, double latency)
+{
+    if (replyStatus == PINGER_STATUS_SUCCESS)
+    {
+        //calculate statistics
+        latencyCalculator.addResponse(latency);
+
+        //update data
+        ui->labelLastRTT->setText(QString::number(latencyCalculator.lastLatency(),'f',2)+" ms");
+        ui->labelLastRTT->setStyleSheet("font: bold 18px;\ncolor: rgb(0, 0, 0);");
+        ui->labelLastRTTLabel->setStyleSheet(STYLE_LABEL_NORMAL);
+        ui->labelAvgRTT->setText(QString::number(latencyCalculator.averageLatency(),'f',2)+" ms");
+        ui->labelAvgJitter->setText(QString::number(latencyCalculator.averageJitter(),'f',2)+" ms");
+        //format jitter
+        if (latencyCalculator.percentJitter() >= JITTER_PERCENT_RED)
+        {
+            ui->labelAvgJitter->setStyleSheet(STYLE_FIELD_RED);
+            ui->labelAvgJitterLabel->setStyleSheet(STYLE_LABEL_RED);
+        }
+        else if (latencyCalculator.percentJitter() >= JITTER_PERCENT_YELLOW)
+        {
+            ui->labelAvgJitter->setStyleSheet(STYLE_FIELD_YELLOW);
+            ui->labelAvgJitterLabel->setStyleSheet(STYLE_LABEL_YELLOW);
+        }
+        else
+        {
+            ui->labelAvgJitter->setStyleSheet(STYLE_FIELD_NORMAL);
+            ui->labelAvgJitterLabel->setStyleSheet(STYLE_LABEL_NORMAL);
+        }
+
+        //update graph
+        this->updateGraph(latencyCalculator.lastLatency());
+    }
+    else
+    {
+        //calculate statistics
+        latencyCalculator.addTimeout();
+
+        //update data
+        if (replyStatus == PINGER_STATUS_TIMEOUT)
+            ui->labelLastRTT->setText("Timeout!");
+        else
+            ui->labelLastRTT->setText("Error!");
+        ui->labelLastRTT->setStyleSheet("font: bold 18px;\ncolor: rgb(153, 0, 0);\nbackground-color: rgb(255, 166, 166);");
+        ui->labelLastRTTLabel->setStyleSheet(STYLE_LABEL_RED);
+
+        //update graph
+        this->updateGraph(std::numeric_limits<double>::quiet_NaN());
+    }
+
+    //set packet loss
+    ui->labelPctLoss->setText(QString::number(latencyCalculator.percentLoss(),'f',2)+" %");
+    //format packet loss
+    if (latencyCalculator.percentLoss() >= LOSS_PERCENT_RED)
+    {
+        ui->labelPctLoss->setStyleSheet(STYLE_FIELD_RED);
+        ui->labelPctLossLabel->setStyleSheet(STYLE_LABEL_RED);
+    }
+    else if (latencyCalculator.percentLoss() >= LOSS_PERCENT_YELLOW)
+    {
+        ui->labelPctLoss->setStyleSheet(STYLE_FIELD_YELLOW);
+        ui->labelPctLossLabel->setStyleSheet(STYLE_LABEL_YELLOW);
+    }
+    else
+    {
+        ui->labelPctLoss->setStyleSheet(STYLE_FIELD_NORMAL);
+        ui->labelPctLossLabel->setStyleSheet(STYLE_LABEL_NORMAL);
+    }
+}
+
+void PingWindow::updateGraph(double lastLatency)
+{
+    double key = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000.0;
+    ui->plotLatency->graph(0)->addData(key, lastLatency);
+    ui->plotLatency->graph(0)->removeDataBefore(key-110);
+    ui->plotLatency->graph(0)->rescaleValueAxis();
+    ui->plotLatency->yAxis->setRangeUpper((double)ui->plotLatency->yAxis->range().upper*1.1);
+    ui->plotLatency->yAxis->setRangeLower(0);
+    ui->plotLatency->xAxis->setRange(key+0.25, 100, Qt::AlignRight);
+    ui->plotLatency->replot();
 }
--- pingwindow.h
+++ pingwindow.h
@@ -8,6 +8,18 @@
 
 #include "hoststreeview.h"
 #include "pinger.h"
+#include "pingstatistics.h"
+
+#define JITTER_PERCENT_YELLOW 15
+#define JITTER_PERCENT_RED 50
+#define LOSS_PERCENT_YELLOW 1
+#define LOSS_PERCENT_RED 30
+#define STYLE_FIELD_RED "font: 12px;\ncolor: rgb(0, 0, 0);\nbackground-color: rgb(255, 166, 166);"
+#define STYLE_FIELD_YELLOW "font: 12px;\ncolor: rgb(0, 0, 0);\nbackground-color: rgb(255, 255, 178);"
+#define STYLE_FIELD_NORMAL "font: 12px;\ncolor: rgb(0, 0, 0);"
+#define STYLE_LABEL_RED "font: 10px;\ncolor: rgb(102, 102, 102);\nbackground-color: rgb(255, 166, 166);"
+#define STYLE_LABEL_YELLOW "font: 10px;\ncolor: rgb(102, 102, 102);\nbackground-color: rgb(255, 255, 178);"
+#define STYLE_LABEL_NORMAL "font: 10px;\ncolor: rgb(102, 102, 102);"
 
 namespace Ui {
 class PingWindow;
@@ -28,13 +40,19 @@ private slots:
     void run(int hostID);
     void lookupHostReply(QHostInfo);
     void asyncTask();
+    void updateUI(int replyStatus, double latency);
 
 private:
     Ui::PingWindow* ui;
     Pinger* pingerInstance;
     QTimer* asyncWorker;
+    PingContext* pigingContext;
     QHostAddress pingingAddress;
-
+    PingStatistics latencyCalculator;
+    bool waitingForReply;
+    bool retryImmediatelly;
+    void resetValues();
+    void updateGraph(double lastLatency);
 };
 
 #endif // PINGWINDOW_H
