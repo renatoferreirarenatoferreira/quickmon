42111d601b505e4cbbc0b0fd81a8a546fbbbb1a4
Renato A. Ferreira <renatoferreirarenatoferreira@hotmail.com>
2015-07-19 11:42:50 -0300

    Reorganizing stylesheet structure.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,6 @@ int main(int argc, char *argv[])
     a.setApplicationName(APPLICATION_NAME);
     a.setApplicationVersion(APPLICATION_VERSION);
 
-    //set style from resource file
-    QFile styleFile(":Styles/Main");
-    styleFile.open(QFile::ReadOnly);
-    QString style(styleFile.readAll());
-    a.setStyleSheet(style);
-
     MainWindow w;
     w.show();
 
